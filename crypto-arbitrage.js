f25da01d51f9d5cf6c44349f6eec38f2baf24242
Berk Erol <b.erol@comforte.com>
2025-01-10 03:19:07 +0100

    Remove bybit from FDUSD-USDT

--- crypto-arbitrage.js
+++ crypto-arbitrage.js
@@ -132,7 +132,7 @@ const EXCHANGES = {
 
 const SYMBOLS = {
   'USDE-USDT': ['bybit', 'bitget', 'kucoin', 'gateio'],
-  'FDUSD-USDT': ['bybit', 'bitget', 'gateio', 'bingx'],
+  'FDUSD-USDT': ['bitget', 'gateio', 'bingx'],
   'USDD-USDT': ['bybit', 'htx', 'gateio', 'coinw'],
   'XAUT-USDT': ['bitget', 'htx', 'gateio', 'bingx', 'coinw'],
   'PAXG-USDT': ['pionex', 'binance', 'bybit', 'bitget', 'kucoin', 'bingx', 'coinw']
